a1e4caa7d4428692f72b2e2f67bac5bbf1ca9320
zhaozz1015 <zhaozz1015@163.com>
2021-01-11 14:13:08 +0800

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def say_hello(world):
+    print('Hello {world}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Eddie')
+    say_hello('William')
