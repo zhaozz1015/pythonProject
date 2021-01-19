6e07e6fef53c8a011e4c98b76db811966e660d79
zhaozz1015 <zhaozz1015@163.com>
2021-01-19 16:18:47 +0800

    规范性检查代码

--- format_check.py
+++ format_check.py
@@ -1,7 +1,11 @@
 #encoding: utf-8
 import pandas as pd
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 import log_util
 import excel_util
+import os
 
 excel_path = "conf\\config.xlsx"
 
@@ -37,13 +41,80 @@ def check_cell_value(expression, square, length_limit):
             if (i >= int(position_star[1]) - 2 and i <= int(position_end[1]) - 2):
                 params.append(excel.ix[i, int(position_star[0])])
 
-    file_url = square[0:square.find('|')]
-    check_square = square[square.find('|') + 1 : len(square)]
+    file_url = unicode(square[0:square.find('|')], 'utf-8')
+    sheet_index = square[square.find('|') + 1 : square.find('!')]
+    check_square = square[square.find('!') + 1 : len(square)]
 
-    print ('file_url:' + file_url + ', check_square:' + check_square)
-    excel = pd.read_excel(excel_path, sheet_name=0)
+    param_star = check_square[0: check_square.find(':')]
+    param_end = check_square[check_square.find(':') + 1: len(check_square)]
+
+    position_star = excel_util.get_position(param_star)
+    position_end = excel_util.get_position(param_end)
+
+    print (str(os.path.isdir(file_url)) + ', file_url:' + file_url + ', sheet_index:' + sheet_index + ', check_square:' + check_square)
+
+    if (os.path.isdir(file_url)):
+        files = os.listdir(file_url)
+        for file in files:
+            check_file_value(expression, params, file_url + file, sheet_index, position_star, position_end)
+    else:
+        check_file_value(expression, params, file_url, sheet_index, position_star, position_end)
+
+def check_file_value(expression, params, file_url, sheet_index, position_star, position_end):
+    check_type = get_check_type(expression)
+
+    print('position_star:' + position_star[0] + ', ' + position_star[1])
+
+    if (check_type == 0) :
+        value_add_check(params, file_url, sheet_index, position_star, position_end)
+    elif (check_type == 1):
+        value_eq_check(params, file_url, sheet_index, position_star, position_end)
+
+def get_check_type(expression):
+    check_type = 0 #默认XX+
+    if (expression.startswith('<|') and expression.endswith('|>')):
+        check_type = 1 #相等
+
+    return check_type
+
+def value_eq_check(params, file_url, sheet_index, position_star, position_end) :
+    excel = pd.read_excel(file_url, sheet_name=int(sheet_index))
+    valid_value = False
+
+    total_count = 0
+    error_count = 0
     for i in excel.index.values:
-        print (excel.ix[i, 0])
+        if (i >= int(position_star[1]) - 2 and i <= int(position_end[1]) - 2):
+            total_count = total_count + 1
+            valid_value = False
+            value = str(excel.ix[i, int(position_star[0])])
+            for x in params:
+                if (value == x):
+                    valid_value = True
 
-    # for x in params:
-    #     print(x)
+            if (not valid_value):
+                error_count = error_count + 1
+                log_util.log_result('异常', '[' + value + ']非有效值。' + file_url)
+
+    log_util.log_report('有效值检查，共计' + str(total_count) + '项，其中异常' + str(error_count) + '项。' + file_url)
+
+def value_add_check(params, file_url, sheet_index, position_star, position_end) :
+    excel = pd.read_excel(file_url, sheet_name=int(sheet_index))
+    valid_value = False
+
+    total_count = 0
+    error_count = 0
+    for i in excel.index.values:
+        if (i >= int(position_star[1]) - 2 and i <= int(position_end[1]) - 2):
+            total_count = total_count + 1
+            valid_value = False
+            value = str(excel.ix[i, int(position_star[0])])
+            for x in params:
+                if (value.startswith(x)):
+                    valid_value = True
+
+            if (not valid_value):
+                error_count = error_count + 1
+                log_util.log_result('异常', '[' + value + ']非动词+结构。' + file_url)
+
+    log_util.log_report('动词+检查，共计' + str(total_count) + '项，其中异常' + str(error_count) + '项。' + file_url)
